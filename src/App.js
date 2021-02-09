b12ff772da6346baa103a4f740aebc4ea6deb1c0
quentintrouve <trouve.quentin@gmail.com>
2021-02-09 20:31:34 +0100

    add anim on mouseDown

--- src/App.js
+++ src/App.js
@@ -10,8 +10,15 @@ function App() {
     pointerRef.current.setAttribute('style', `top:${e.pageY - 20}px; left:${e.pageX - 20}px;`)
   }
 
+  const addClass = () => {
+    pointerRef.current.classList.add('expand');
+  }
+  const removeClass = () => {
+    pointerRef.current.classList.remove('expand');
+  }
+
   return (
-    <div onMouseMove={pointerMove} className="App">
+    <div onMouseDown={addClass} onMouseUp={removeClass} onMouseMove={pointerMove} className="App">
       <div ref={pointerRef} className="cursor-custom"></div>
       <Onglet />
     </div>
