b3f29ab312c64c1dec386dac7504ad320acf3cd6
quentintrouve <trouve.quentin@gmail.com>
2021-02-09 20:15:29 +0100

    add useRef for a custom pointer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useRef } from 'react'
 import Onglet from './components/Onglet/Onglet'
+import "./App.css"
 
 function App() {
+
+  let pointerRef = useRef();
+
+  const pointerMove = e => {
+    pointerRef.current.setAttribute('style', `top:${e.pageY - 20}px; left:${e.pageX - 20}px;`)
+  }
+
   return (
-    <div className="App">
+    <div onMouseMove={pointerMove} className="App">
+      <div ref={pointerRef} className="cursor-custom"></div>
       <Onglet />
     </div>
   );
